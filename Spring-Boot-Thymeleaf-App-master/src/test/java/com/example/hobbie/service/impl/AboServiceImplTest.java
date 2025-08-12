a327d87162ce932473acb02afa1d81a49d650ee9
Swadesh1212 <swadeshsamantaray74@gmail.com>
2025-08-13 00:22:09 +0530

    updated

--- Spring-Boot-Thymeleaf-App-master/src/test/java/com/example/hobbie/service/impl/AboServiceImplTest.java
+++ Spring-Boot-Thymeleaf-App-master/src/test/java/com/example/hobbie/service/impl/AboServiceImplTest.java
@@ -21,7 +21,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.BDDAssumptions.given;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyVararg;
+
 import static org.mockito.Mockito.*;
 
 class AboServiceImplTest {
